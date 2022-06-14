f68489c4bfd3cfcd34126bf703d3abb9b21d3079
Neelutpal Saha <neelutpal.saha@s.amity.edu>
2022-06-14 16:46:18 +0000

    add states

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./styles/App.css";
 
 function App() {
+  const [maxCards, setMaxCards] = useState(5);
+  const [cards, setCards] = useState();
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [displayText, setDisplayText] = useState("");
+
   return <div className="App"></div>;
 }
 
